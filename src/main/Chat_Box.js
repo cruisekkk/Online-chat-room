2e4452e478bea7d16661eb0e350877f7099bd804
mengyi <cruise_lee@outlook.com>
2020-11-02 00:28:09 +0800

    feat: socket.io basic message config, server push

---
+++ src/main/Chat_Box.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./Room.css";
+
+class ChatBox extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue : '',
+    }
+    this.inputRef = React.createRef();
+  }
+
+  emitMessage(e) {
+    e.preventDefault();
+    // console.log(e.target);
+    console.log(this.state.inputValue);
+    this.props.onSend(this.state.inputValue);
+    this.setState({inputValue: ''});
+    console.log(this.inputRef);
+    this.inputRef.current.value = '';
+  }
+  bindValue(e){
+    this.setState({inputValue: e.target.value});
+    console.log(this.props.messages);
+  }
+  render(){
+    return(
+      <div className="ChatBox">
+        <ul id="messages">
+          {this.props.messages.map((item) => {
+            return (<li>{item}</li>)
+          })}
+        </ul>
+        <form action="" onSubmit={(e) => this.emitMessage(e)}>
+          <input id="m" ref={this.inputRef}onChange={(e) => this.bindValue(e)}/><button>Send</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ChatBox;
--- src/main/Room.js
+++ src/main/Room.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
-import Loading from "../components/Loading"
+import Loading from "../components/Loading";
+import ChatBox from "./Chat_Box";
+import io, { Socket } from "socket.io-client";
 class Room extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      ws: null,
+      messages: [],
+    }
+  }
+
+  componentDidMount() {
+    let ws = io('http://localhost:8000');
+    this.setState({ws}, () => {
+      this.state.ws.on('chat message', (msg) => {
+        console.log(msg);
+        this.setState({messages: this.state.messages.concat([msg])});
+        console.log("receive chat message");
+      })
+    });
+    console.log("invoked");
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    return true;
+  }
+  componentDidUpdate(nextProps, nextState) {
+    
+  }
+  sendMessage(message){
+    this.state.ws.emit('chat message', message);
+  }
   render(){
     return(
       <div className="Room">
         <Loading/>
+        <ChatBox onSend={this.sendMessage.bind(this)} messages={this.state.messages}/>
       </div>
     );
   }
