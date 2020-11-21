592cad24e59a72b4cf76dd43d1444d545a54bd1a
mengyi <cruise_lee@outlook.com>
2020-11-22 01:12:33 +0800

    try router, no progress, found webpack devserver & browserRouter conflict

--- src/App.js
+++ src/App.js
@@ -5,36 +5,140 @@ import {
   Switch,
   Route,
   Link,
-  Redirect
+  Redirect,
+  useHistory,
+  useLocation
 } from "react-router-dom";
 import Room from "./main/Room";
-import LogIn from "./main/LogIn";
+// import LogIn from "./main/LogIn";
 import "./App.css";
+
+const authContext = createContext();
+
+const fakeAuth = {
+  isAuthenticated: false,
+  signin(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+function ProvideAuth({children}){
+  const auth = useProvideAuth();
+  return (
+    <authContext.Provider value={auth}>
+      {children}
+    </authContext.Provider>
+  );
+}
+
+function useAuth() {
+  return useContext(authContext);
+}
+
+function useProvideAuth() {
+  const [user, setUser] = useState(null);
+  
+  const signin = cb => {
+    return fakeAuth.signin(() => {
+      setUser("user");
+      cb();
+    });
+  };
+
+  const signout = cb => {
+    return fakeAuth.signout(() => {
+      setUser(null);
+      cb();
+    })
+  }
+
+  return {
+    user,
+    signin,
+    signout
+  }
+}
+
+function AuthButton() {
+  let history = useHistory();
+  let auth = useAuth();
+
+  return auth.user ? (
+    <p>
+      Welcome! {" "}
+      <button
+        onClick={() => {
+          auth.signout(() => history.push("/"));
+        }}
+      >
+        Sign out
+      </button>
+    </p>
+  ) : (
+    <p>
+      You are not logged in.
+    </p>
+  )
+}
+
+function PrivateRoute({ children, ...rest}) {
+  let auth = useAuth();
+  console.log(1);
+  console.log(auth.user);
+  return (
+    <Route 
+      {...rest}
+      render={({ location }) => {
+        auth.user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/LogIn",
+              state: { from: location}
+            }}
+          />
+        )
+      }}
+    />
+  );
+}
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
   }, [loggedIn])
+
   return(
-    <Router>
-    <div className="App">
-    <button onClick={() => setLoggedIn(true)}>
-        Click me
-    </button>
-      <Switch>
-        <Route path="/" exact>
-          { loggedIn ? <Redirect to="/Room"/> : <Home/> }
-        </Route>
-        <Route path="/LogIn" exact>
-          <LogIn />
-        </Route>
-        <Route path="/Room" exact>
-          <Room />  
-        </Route>
-      </Switch>
-    </div>
-    </Router>
+    <ProvideAuth>
+      <Router>
+      <div className="App">
+      <button onClick={() => setLoggedIn(true)}>
+          Click me
+      </button>
+        <Switch>
+          {/* <PrivateRoute path="/" exact>
+            <Home/> 
+          </PrivateRoute> */}
+          <Route path="/" exact>
+            {/* <Redirect to="/Room" /> */}
+            </Route>
+          <Route component={LogIn} path="/Room" exact>
+            <LogIn />
+          </Route>
+          {/* <PrivateRoute path="/Room" exact>
+            <Room />  
+          </PrivateRoute> */}
+        </Switch>
+      </div>
+      </Router>
+     </ProvideAuth>
   );
 }
 
@@ -51,6 +155,27 @@ function Home() {
       </ul>
     </nav>
   )
+}
+
+function LogIn() {
+  console.log(222);
+  let history = useHistory();
+  let location = useLocation();
+  let auth = useAuth();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    auth.signin(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
 }
 
 export default hot(module)(App);
