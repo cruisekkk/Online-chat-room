47dfe8149aa3a4946b5e889ccb4b7ac7529019c3
mengyi <cruise_lee@outlook.com>
2020-11-27 23:34:53 +0800

    feat: set up db connection, refactor

--- node_server/index.js
+++ node_server/index.js
@@ -1,13 +1,30 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const cors = require('cors');
+const bodyParser = require("body-parser")
+const checkAccount = require("../test") // node 没法用import
 
-app.get('/', (req, res) => {
-  // res.sendFile(__dirname + '/public/index.html');
+app.use(cors())
+app.use(bodyParser.json());// 添加json解析
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/login', async (req, res) => {
+  console.log("login area");
+  console.log(req.body);
+  // console.log();
+  // console.log(checkAccount);
+  console.log("res");
+  // console.log(checkAccount(req.body.email, req.body.password))
+  checkAccount(req.body.email, req.body.password).then( (result) => {
+    console.log("result!!!")
+    console.log(result);
+    res.send(result);
+  })
 });
 
 io.on('connection', (socket) => {
-  console.log(socket);
+  // console.log(socket);
   console.log('a user connected');
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
